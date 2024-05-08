04484d0a15c746700973309e3f4596d2afee7e79
Lautaro Salazar <salazarlautaroalejandro@hotmail.com>
2024-05-08 00:05:19 -0300

    nueva rama

---
+++ src/main/java/com/tallerwebi/dominio/ServicioTest.java
@@ -0,0 +1,7 @@
+package com.tallerwebi.dominio;
+import com.tallerwebi.presentacion.RespuestasTest;
+
+public interface ServicioTest {
+    String calcularResultado(RespuestasTest respuestasTest);
+}
+
---
+++ src/main/java/com/tallerwebi/presentacion/ControladorSuscripcion.java
@@ -0,0 +1,14 @@
+package com.tallerwebi.presentacion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorSuscripcion {
+
+    @RequestMapping("/suscripcion")
+    public ModelAndView irASuscripcion() {
+        return new ModelAndView("suscripcion");
+    }
+}
