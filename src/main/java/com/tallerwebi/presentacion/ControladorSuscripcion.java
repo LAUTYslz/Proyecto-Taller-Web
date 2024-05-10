3822e582f764f96e5dbcf41bab4f491ed084cee2
leitesbianca <143121694+leitesbianca@users.noreply.github.com>
2024-05-09 22:45:55 -0300

    avancé con la parte de membresia paga

--- src/main/java/com/tallerwebi/presentacion/ControladorSuscripcion.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorSuscripcion.java
@@ -1,6 +1,10 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.Usuario;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,4 +15,25 @@ public class ControladorSuscripcion {
     public ModelAndView irASuscripcion() {
         return new ModelAndView("suscripcion");
     }
+
+    @RequestMapping("/membresiaPaga")
+    public ModelAndView irAMembresiaPaga() {
+        return new ModelAndView("membresiaPaga");
+    }
+
+    @RequestMapping(path = "/membresiaPaga")
+    public ModelAndView nuevaMembresia() {
+        ModelMap model = new ModelMap();
+        model.put("nuevaMembresiaPaga", new DatosMembresia());
+        return new ModelAndView("nuevo-usuario", model);
+    }
+
+    @GetMapping("/membresiaPaga")
+    public ModelAndView procesarDatosDeMembresiaPaga(@ModelAttribute("datosMembresia") DatosMembresia datosMembresia){
+        ModelMap model = new ModelMap();
+        model.put("datosMembresia", datosMembresia);
+        return new ModelAndView("confirmacionMembresia", model);
+    }
+
+
 }
