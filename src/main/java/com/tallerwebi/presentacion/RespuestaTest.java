4ebd758e3d58b68dee34537712f9b4787ee56af6
ANA LAURA NUnEZ <ananunez@alumno.unlam.edu.ar>
2024-05-30 01:51:56 -0300

    Maquetado tests

---
+++ src/main/java/com/tallerwebi/presentacion/RespuestaTest.java
@@ -0,0 +1,20 @@
+package com.tallerwebi.presentacion;
+
+public class RespuestaTest {
+    private String texto;
+
+    public RespuestaTest() {
+    }
+
+    public RespuestaTest(String texto) {
+        this.texto = texto;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
